d9ee1f364103da05e72a831e1402e9f9ca2a7905
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2017-12-02 21:21:47 +0000

    Kotlin day2 part1

--- Kotlin/day2_part1.kt
+++ Kotlin/day2_part1.kt
@@ -1,5 +1,17 @@
-import com.opencsv.CSVReader
+import com.opencsv.CSVParserBuilder
+import com.opencsv.CSVReaderBuilder
+import java.io.*
+import java.util.*
 
 fun main(args: Array<String>) {
-    println("test")
+    val parser = CSVParserBuilder().withSeparator('\t').withIgnoreQuotations(true).build()
+    val reader = CSVReaderBuilder(FileReader("../day2_input.txt")).withCSVParser(parser).build()
+    var total = 0
+    while(true) {
+        val row = reader.readNext()?.map { it.toInt() } ?: break
+        val max = row.max()
+        val min = row.min()
+        total += max!!-min!!
+    }
+    println(total)
 }
