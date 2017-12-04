15026bb4cd4f8ed69bc65edcdb67500b3eb7aa54
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2017-12-04 00:29:09 +0000

    strconv be gone!

--- Go/day1_part1.go
+++ Go/day1_part1.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"strconv"
 )
 
 func day1Part1() {
@@ -12,8 +11,7 @@ func day1Part1() {
 	total := 0
 	for i, curr := range s {
 		if curr == rune(s[(i+1)%len(s)]) {
-			num, _ := strconv.Atoi(string(curr))
-			total += num
+			total += int(curr - '0')
 		}
 	}
 	fmt.Println(total)
--- Go/day1_part2.go
+++ Go/day1_part2.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"strconv"
 )
 
 func day1Part2() {
@@ -12,8 +11,7 @@ func day1Part2() {
 	total := 0
 	for i, curr := range s {
 		if curr == rune(s[(i+len(s)/2)%len(s)]) {
-			num, _ := strconv.Atoi(string(curr))
-			total += num
+			total += int(curr - '0')			
 		}
 	}
 	fmt.Println(total)
